3b0b6ef9477a05a49e5f5e0840b0d7395a172837
mamdouh <mo.mamdouh.saad@gmail.com>
2020-05-25 07:40:34 +0200

    post-list and post

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageContainerComponent } from './main-page-container/main-page-container.component';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostComponent } from './components/post/post.component';
 
 
 @NgModule({
-  declarations: [MainPageContainerComponent, HomeBannerComponent],
+  declarations: [MainPageContainerComponent, HomeBannerComponent, PostListComponent, PostComponent],
   imports: [
     CommonModule,
     MainRoutingModule
